b756aa18856f6ea206f5ec2ba51e1abf4d1566f5
j-knudson <knudsonjd@gmail.com>
2021-10-12 16:35:06 -0500

    Program cycles through both stacks returning movies    rand() not working correctly to select a random movie

--- Movie Night/Movie Night/MovieNightMain.cpp
+++ Movie Night/Movie Night/MovieNightMain.cpp
@@ -13,6 +13,8 @@ Randomly select and display a title to the user*/
 void OpenInputFile(std::ifstream& in_f);
 //Postcondition: A file stream has been opened for input
 
+std::string GetRandomTitle(Stack &source, int random_max);
+
 
 
 
@@ -24,9 +26,9 @@ int main()
 
     Stack OddMovieStack, EvenMovieStack, TempStack; //create three stacks to hold movies 
 
-    //***** test variable
-    int count = 0; 
-    //**** test variable 
+    
+    int count = 0;                  //variable to help determine how many preceeeding characters need to be consumed before saving movie title string 
+    
 
     while (!infile.eof()) {
         std::string line;   //create a variable to read a line of the file 
@@ -52,24 +54,61 @@ int main()
         count++;
     }
 
-    std::cout << count<< std::endl;
+    
     size_t odd_stack_count = OddMovieStack.size(); 
     size_t even_stack_count = EvenMovieStack.size(); 
 
-    //test
+    //test ***********************
     std::cout << odd_stack_count << std::endl; 
     std::cout << even_stack_count << std::endl;
+    //end test *******************************
 
-    while (OddMovieStack.size() > 0) {
+
+    /*while (OddMovieStack.size() > 0) {
         std::string title;
         title = OddMovieStack.pop(); 
         std::cout << title <<std::endl; 
+        odd_stack_count--; 
     }
 
-    while (EvenMovieStack.size() > 0) {
+    /*while (EvenMovieStack.size() > 0) {
         std::string title;
         title = EvenMovieStack.pop();
         std::cout << title << std::endl;
+    }*/
+
+    std::string random_movie;
+    while (odd_stack_count > 0 || even_stack_count > 0) {
+     
+        std::cout << "Please input a number used to randomly select a movie title: \n";     //get a random number from user 
+        int user_random_number;  std::cin >> user_random_number; 
+
+        if (odd_stack_count && even_stack_count > 0) {              //first condition if there are movies in both the even and odd movie stack
+            if (user_random_number % 2 == 0) {                      //if the user number indicates pulling from the even stack
+                random_movie = GetRandomTitle(EvenMovieStack, EvenMovieStack.size());
+                even_stack_count--;
+            }
+            else {                                                  //if the user number indicates pulling from the odd stack
+                random_movie = GetRandomTitle(OddMovieStack, OddMovieStack.size());
+                odd_stack_count--; 
+            }
+        }
+
+        else if (odd_stack_count > 0) {                             //second condition: only odd stack titles available
+            random_movie = GetRandomTitle(OddMovieStack, OddMovieStack.size());
+            odd_stack_count--; 
+        }
+
+        else {                                                      //third condition: only even stack titles available
+            random_movie = GetRandomTitle(EvenMovieStack, EvenMovieStack.size());
+            even_stack_count--;
+        }
+            
+        std::cout << "Your surprise movie is:  " << random_movie << std::endl;
+    }
+
+    if (odd_stack_count == 0 && even_stack_count == 0) {
+        std::cout << "No other available films \n";
     }
 
 
@@ -88,4 +127,20 @@ void OpenInputFile(std::ifstream& in_f)    //function to open an input file
         std::cin >> input_filename;
         in_f.open(input_filename.c_str());
     } while (in_f.fail());
+}
+
+
+std::string GetRandomTitle(Stack &source, int random_max)
+{
+    int selection = rand() % random_max + 1;   
+    Stack temp;
+    while (source.size() > selection) {
+        temp.push(source.pop());
+    }
+    std::string target = source.pop(); 
+    while (temp.size() > 0) {
+        source.push(temp.pop());
+    }
+    //std::cout << "Function found " << target <<std::endl; 
+    return target; 
 }
